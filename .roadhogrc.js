b1b838f92a732523b261f591b88a5562bcc57709
nowgoant <majun1@jd.com>
2017-07-18 19:31:05 +0800

    init

---
+++ .roadhogrc.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+export default {
+  entry: 'src/index.js',
+  // 接口代理示例
+  "proxy": {
+    "/api": {
+      "target": "http://jsonplaceholder.typicode.com/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api": "" }
+    }
+  },
+  "env": {
+    "development": {
+      "extraBabelPlugins": [
+        "dva-hmr",
+        "transform-runtime",
+        ["import", { "libraryName": "antd", "style": true }]
+      ]
+    },
+    "production": {
+      "extraBabelPlugins": [
+        "transform-runtime",
+        ["import", { "libraryName": "antd", "style": true }]
+      ]
+    }
+  }
+}
