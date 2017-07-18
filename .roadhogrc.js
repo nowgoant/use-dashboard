d69aa8c4da39fbd05014154f4680dd48bc8b6e43
nowgoant <majun1@jd.com>
2017-07-18 20:09:40 +0800

    update

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -2,6 +2,16 @@ const path = require('path')
 
 export default {
   entry: 'src/index.js',
+  "disableCSSModules": false,
+  "publicPath": "/",
+  "theme": {
+    "@primary-color": "#1DA57A",
+    "@link-color": "#1DA57A",
+    "@border-radius-base": "2px",
+    "@font-size-base": "16px",
+    "@line-height-base": "1.2"
+  },
+  "autoprefixer": null,
   // 接口代理示例
   "proxy": {
     "/api": {
@@ -24,5 +34,15 @@ export default {
         ["import", { "libraryName": "antd", "style": true }]
       ]
     }
+  },
+  "dllPlugin": {
+    "exclude": [
+      "babel-runtime"
+    ],
+    "include": [
+      "dva/router",
+      "dva/saga",
+      "dva/fetch"
+    ]
   }
 }
